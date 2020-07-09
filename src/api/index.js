7fdf1a5ee9dd2bad51f1773a70b40091570914ba
Daniyal Manzoor <daniyalmanzoor28@gmail.com>
2020-07-09 12:32:06 +0500

    add cards of infected, confirmed, deaths

--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,12 @@
 import axios from "axios";
-const url = "https://covid19.methdro.id/api";
+const url = "https://covid19.mathdro.id/api";
 
 export const fetchData = async () => {
   try {
     const {
-      data: { comfirmed, recovered, deaths, lastUpdate }
+      data: { confirmed, recovered, deaths, lastUpdate }
     } = await axios.get(url);
-    return { comfirmed, recovered, deaths, lastUpdate };
+    return { confirmed, recovered, deaths, lastUpdate };
   } catch (error) {}
 };
 
